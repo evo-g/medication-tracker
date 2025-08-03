1b72c695f340a6b8290a33064e03f037b6b080f0
evo-g <evodiog@gmail.com>
2025-08-03 00:19:41 -0700

    updates issue with pulling more published records

--- lib/graphql/queries/getMedicationFormsByDate.ts
+++ lib/graphql/queries/getMedicationFormsByDate.ts
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 
 export const GET_MED_FORMS_BY_DATE = gql`
   query GetMedFormsByDate($date: Date!) {
-    medicationForms(where: { date: $date }) {
+    medicationForms(where: { date: $date }, first: 100) {
       id
       date
       timeGiven
